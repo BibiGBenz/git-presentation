ee385e1e19bfa3180cf1f218b832ae737cfb59db
BibiGBenz <bibigbenz123@gmail.com>
2023-09-12 15:33:22 +0530

    drver class done

--- calci.java
+++ calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface calci
 {
 	int add(int a,int b);
@@ -16,5 +17,18 @@ public class Demo{
 	public int multi(int a, int b)
 	{
 		return a*b;
+	}
+}
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		Demo D=new Demo();
+		System.out.println(a+"+"+b+"="D.add(a,b));
+		System.out.println(a+"-"+b+"="D.sub(a,b));
+		System.out.println(a+"*"+b+"="D.multi(a,b));
 	}
 }
